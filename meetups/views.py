9a4e3c2edc8fe517a3663d420ceca5df920612f7
vinivosh <vinicius7427@gmail.com>
2022-03-16 19:23:38 -0300

    Unleashing more of DTL's power!
    DTL being the Django Template Language.
    
    It is ugly as heck, but it gets the job done, I guess 💀

--- meetups/views.py
+++ meetups/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import meetups
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'meetups/index.html')
+    meetups = [
+        {'title': 'Family Reunion'},
+        {'title': 'Coffee Break 1.0'},
+        {'title': 'Intervention For Our Dear Friend'},
+        {'title': 'Coffee Break 2.0'}
+    ]
+
+    return render(request, 'meetups/index.html', {
+        'showMeetups': True,
+        'meetups': meetups
+    })
