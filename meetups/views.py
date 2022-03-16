5ae215b44b13e3d0354113a96cda5b60d56fddd6
vinivosh <vinicius7427@gmail.com>
2022-03-16 20:15:09 -0300

    An actually decent dynamic index.html is now up 😊
    Thanks Max for your CSS 👀

--- meetups/views.py
+++ meetups/views.py
@@ -7,10 +7,26 @@ import meetups
 
 def index(request):
     meetups = [
-        {'title': 'Family Reunion'},
-        {'title': 'Coffee Break 1.0'},
-        {'title': 'Intervention For Our Dear Friend'},
-        {'title': 'Coffee Break 2.0'}
+        {
+            'title': 'Family Reunion',
+            'location': 'Itaporã',
+            'slug': 'family-reunion-itapora'
+        },
+        {
+            'title': 'Coffee Break 1.0',
+            'location': 'Pindamonhangama',
+            'slug': 'coffee-break-pindamonhangaba'
+        },
+        {
+            'title': 'Intervention For Our Dear Friend',
+            'location': 'Patos de Minas',
+            'slug': 'intervention-for-our-dear-friend-patos-de-minas'
+        },
+        {
+            'title': 'Coffee Break 2.0',
+            'location': 'Chuí',
+            'slug': 'coffee-break-chui'
+        }
     ]
 
     return render(request, 'meetups/index.html', {
