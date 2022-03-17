77ea94b7ccc3fd5c6e445ca30fcaf1e057f88d73
vinivosh <vinicius7427@gmail.com>
2022-03-17 14:19:50 -0300

    Detail page now does load at meetups/<a-slug-here>

--- meetups/urls.py
+++ meetups/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index)    # my-domain.com/meetups
+    path('meetups/', views.index),    # my-domain.com/meetups
+    path('meetups/<slug:meetupSlug>', views.meetupDetails)    # my-domain.com/meetups/<a-dynamic-segment>
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -34,13 +34,14 @@ def index(request):
         'meetups': meetups
     })
 
-def meetupDetails(request):
+def meetupDetails(request, meetupSlug):
+    print('#DEBUG\n' + meetupSlug + '\n\n')
     selectedMeetup = {
         'title': 'Family Reunion',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis urna id volutpat lacus laoreet non curabitur. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Nunc mattis enim ut tellus elementum. Sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus. Eget egestas purus viverra accumsan. Scelerisque varius morbi enim nunc faucibus a. Nisi vitae suscipit tellus mauris a diam maecenas sed. Viverra orci sagittis eu volutpat odio facilisis. Suspendisse in est ante in nibh mauris cursus.\nSed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Aliquet sagittis id consectetur purus ut. Vel facilisis volutpat est velit. Imperdiet proin fermentum leo vel orci porta non pulvinar. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Aliquet risus feugiat in ante metus dictum at tempor. In cursus turpis massa tincidunt dui ut ornare lectus. Massa ultricies mi quis hendrerit dolor magna eget est lorem. Id semper risus in hendrerit. Lorem ipsum dolor sit amet consectetur adipiscing elit. Nibh mauris cursus mattis molestie a iaculis at erat. Nibh mauris cursus mattis molestie a iaculis at erat pellentesque. Posuere lorem ipsum dolor sit. Sed felis eget velit aliquet sagittis id consectetur purus ut. Placerat duis ultricies lacus sed turpis tincidunt id aliquet risus. Placerat vestibulum lectus mauris ultrices eros in cursus turpis. Lacus sed viverra tellus in hac habitasse. Morbi tincidunt ornare massa eget egestas purus viverra. Lectus nulla at volutpat diam ut venenatis tellus in. Elementum eu facilisis sed odio morbi.'
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis urna id volutpat lacus laoreet non curabitur. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Nunc mattis enim ut tellus elementum. Sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus. Eget egestas purus viverra accumsan. Scelerisque varius morbi enim nunc faucibus a. Nisi vitae suscipit tellus mauris a diam maecenas sed. Viverra orci sagittis eu volutpat odio facilisis. Suspendisse in est ante in nibh mauris cursus. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Aliquet sagittis id consectetur purus ut. Vel facilisis volutpat est velit. Imperdiet proin fermentum leo vel orci porta non pulvinar. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Aliquet risus feugiat in ante metus dictum at tempor. In cursus turpis massa tincidunt dui ut ornare lectus.'
         }
 
     return render(request, 'meetups/meetupDetails.html', {
         'meetupTitle': selectedMeetup['title'],
-        'meetupDescription': selectedMeetup['Description']
+        'meetupDescription': selectedMeetup['description']
         })
