b9222485d02601e15cc7c48cb129dbc39d719d37
vinivosh <vinicius7427@gmail.com>
2022-03-24 12:16:06 -0300

    Init database
    Creating my first model, the Meetup table!

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,9 @@
+from pydoc import describe
 from django.db import models
 
 # Create your models here.
+
+class Meetup(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
