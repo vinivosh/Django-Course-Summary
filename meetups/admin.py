b55d1377b6af104bc02b3f89b225c6eecb762483
vinivosh <vinicius7427@gmail.com>
2022-03-30 09:18:39 -0300

    Create new table: Location
    Brand new table in the DB: Location.
    
    This will, obviously, store the locations for the meetups. A one-to-many relationship between meetup and location, naturally.

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
-from .models import Meetup
+from .models import Meetup, Location
 
 
 
 class MeetupAdmin(admin.ModelAdmin):
     list_display =('title', 'slug')
-    # list_filter = ('title',)
+    list_filter = ('location',)
     # This prepopulates the slug field, based on the title, when creating a new Meetup in the DB
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
--- meetups/models.py
+++ meetups/models.py
@@ -3,12 +3,22 @@ from django.db import models
 
 
 
+class Location(models.Model):
+    city = models.CharField(max_length=120)
+    province = models.CharField(max_length=120)
+    country = models.CharField(max_length=60)
+    fullAdress = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.city} - {self.province} ({self.fullAdress})'
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images', default='defaultImage.jpg')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, default=None)
 
     # Defining this will change how an object of this class will be converted to a string via the default python function str(). Cool!
-    def __str__(self) -> str:
-        return f'{self.title} - {self.slug}'
+    def __str__(self):
+        return f'{self.title} ({self.slug})'
