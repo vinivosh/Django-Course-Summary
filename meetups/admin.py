85e5f32014842ed30b1670573f215318d28ec57f
vinivosh <vinicius7427@gmail.com>
2022-03-30 09:23:36 -0300

    Create table "Participant"
    Brand new table in the DB: Participant.
    
    It will, duh, store the participants for the various meetups. All it needs is an e-mail for now. A many-to-many relationship with Meetup, of course.

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Meetup, Location
+from .models import Meetup, Location, Participant
 
 
 
@@ -12,3 +12,4 @@ class MeetupAdmin(admin.ModelAdmin):
 
 admin.site.register(Meetup, MeetupAdmin)
 admin.site.register(Location)
+admin.site.register(Participant)
--- meetups/models.py
+++ meetups/models.py
@@ -12,12 +12,19 @@ class Location(models.Model):
     def __str__(self):
         return f'{self.city} - {self.province} ({self.fullAdress})'
 
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images', default='defaultImage.jpg')
     location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, default=None)
+    participants = models.ManyToManyField(Participant, blank=True)
 
     # Defining this will change how an object of this class will be converted to a string via the default python function str(). Cool!
     def __str__(self):
