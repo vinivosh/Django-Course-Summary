10cb89abdbf1eb9c8a6fee7c9dd8173aa98af177
vinivosh <vinicius7427@gmail.com>
2022-03-31 09:48:22 -0300

    Add form submission
    Add the user submitted info to the DB if valid
    
    Now the form actually works to add a participant to a meetup.
    
    Annoying limitation: can't add the same e-mail to different meetups as of yet. Will be corrected later.

--- meetups/views.py
+++ meetups/views.py
@@ -20,13 +20,25 @@ def index(request):
 def meetupDetails(request, meetupSlug):
     try:
         selectedMeetup = Meetup.objects.get(slug=meetupSlug)
-        participantForm = ParticipantForm()
+
+        if request.method == 'GET':
+            participantForm = ParticipantForm()
+        else:
+            # There's an annoying limitation on this form: you can't use the same e-mail to enter different meetups for now. This will be corrected later.
+            participantForm = ParticipantForm(request.POST)
+
+            # If the form entered by the user is valid, save the participant to the DB!
+            if participantForm.is_valid():
+                # Saving the participant to the Participant table and to a variable
+                participant = participantForm.save()
+                # Adding the participant to the "participants" field in the current meetup
+                selectedMeetup.participants.add(participant)
 
         return render(request, 'meetups/meetupDetails.html', {
             'meetupFound': True,
             'meetup': selectedMeetup,
             'form': participantForm
-            })
+        })
     except Exception as exc:
         return render(request, 'meetups/meetupDetails.html', {
             'meetupFound': False
